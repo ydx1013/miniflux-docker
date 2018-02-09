d15c7f382d973e5e9ce75343fe9d0e16041dda92
yulahuyed <yulahuyed@vpsorg.top>
2018-02-09 21:56:15 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,10 @@
 export LISTEN_ADDR=0.0.0.0:$PORT
 echo $DATABASE_URL
 
-/usr/local/bin/miniflux -migrate
+miniflux -migrate
 
 expect <<END
-spawn /usr/local/bin/miniflux -create-admin
+spawn miniflux -create-admin
 expect "Username:"
 send "$ADMIN_USER\n"
 expect "Password:"
@@ -14,4 +14,4 @@ send "$ADMIN_PASS\n"
 expect eof
 END
 
-/usr/local/bin/miniflux
+miniflux
